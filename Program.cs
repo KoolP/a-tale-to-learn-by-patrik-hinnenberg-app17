c7cda5cec1946ec809f41de0f9f5ecdb3a09e829
KoolP <patrik.rikama@gmail.com>
2018-11-06 19:04:54 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ataletolearnbypatrikhinnenbergapp17
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
